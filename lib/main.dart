00e91bbd1091b3fbefc31f5f9ea6a9cc027ee46f
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-08-27 18:13:49 +0600

    splahs screen ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanager/ui/screens/login_screen.dart';
 import 'package:taskmanager/ui/screens/splash_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class TaskManager extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: LoginScreen(),
     );
   }
 }
