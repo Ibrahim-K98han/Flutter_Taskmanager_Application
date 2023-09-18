bd4515eda691a425fdeee1c695bb315333ad9820
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-09-18 19:15:44 +0600

    completed task api integrated done

--- lib/ui/screens/completed_task_screen.dart
+++ lib/ui/screens/completed_task_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import '../../data/models/task_model.dart';
+import '../../data/network_utils.dart';
+import '../../data/urls.dart';
+import '../widgets/dashboard_item_widget.dart';
 import '../widgets/screen_background.dart';
+import '../widgets/snackba_message.dart';
 import '../widgets/task_list_item.dart';
 
 class CompletedTaskScreen extends StatefulWidget {
@@ -10,26 +15,90 @@ class CompletedTaskScreen extends StatefulWidget {
 }
 
 class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
+  TaskModel completedTaskModel = TaskModel();
+  bool inProgress = false;
+  @override
+  void initState() {
+    getAllCompletedTasks();
+    super.initState();
+  }
+
+  Future<void> getAllCompletedTasks() async {
+    inProgress = true;
+    setState(() {});
+    final response = await NetworkUtils().getMethod(
+      Urls.completedTaskUrl,
+    );
+    if (response != null) {
+      completedTaskModel = TaskModel.fromJson(response);
+    } else {
+      showSnackBarMessage(context, 'Unable to fetch new task! try again', true);
+    }
+    inProgress = false;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackgroun(
         child: Column(
           children: [
+            Row(
+              children: [
+                Expanded(
+                  child: DashboardItemWidget(
+                    numberOfTask: 12,
+                    typeOfTask: 'New',
+                  ),
+                ),
+                Expanded(
+                  child: DashboardItemWidget(
+                    numberOfTask: 12,
+                    typeOfTask: 'Progress',
+                  ),
+                ),
+                Expanded(
+                  child: DashboardItemWidget(
+                    numberOfTask: 12,
+                    typeOfTask: 'Completed',
+                  ),
+                ),
+                Expanded(
+                  child: DashboardItemWidget(
+                    numberOfTask: 12,
+                    typeOfTask: 'Canceled',
+                  ),
+                ),
+              ],
+            ),
             Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return TaskListItem(
-                    subject: 'Hello',
-                    description: 'lorem Ipsum Doler side',
-                    date: '31/08/2023',
-                    type: 'Completed',
-                    onDeletePress: () {},
-                    onEditPress: () {},
-                  );
-                },
-              ),
+              child: inProgress
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : RefreshIndicator(
+                      onRefresh: () async {
+                        await getAllCompletedTasks();
+                      },
+                      child: ListView.builder(
+                        itemCount: completedTaskModel.data!.length,
+                        itemBuilder: (context, index) {
+                          return TaskListItem(
+                            type: 'Completed',
+                            date: completedTaskModel.data![index].createdDate ??
+                                'Unknown',
+                            description:
+                                completedTaskModel.data![index].description ??
+                                    'Unknown',
+                            subject: completedTaskModel.data![index].title ??
+                                'Unknown',
+                            onDeletePress: () {},
+                            onEditPress: () {},
+                          );
+                        },
+                      ),
+                    ),
             )
           ],
         ),
